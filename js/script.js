59b16c1b0df0b5999758e9a6ccb209bfb14cca1c
7stars993 <bintangwidya2233@gmail.com>
2023-06-07 18:49:35 +0700

    memperbaiki url dan menambah class active pada menu

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 // ## NAVBAR FIXED ##
 // Ketika window/halaman di scroll
 window.onscroll = function () {
-  const menu = document.querySelector(".menu"); // ambil elemen class menu
+  const menu = document.querySelector(".menu"); // ambil elemen dengan nama class menu
   const fixedNav = menu.offsetTop; // ambil jarak dari atas hingga elemen class menu
 
   // Apabila jarak menu bertambah (melebihi fixedNav)
   if (window.pageYOffset > fixedNav) {
-    menu.classList.add("menu-fixed"); // menambah class menu-fixed
+    menu.classList.add("menu-fixed"); // tambah class menu-fixed
   } else {
-    menu.classList.remove("menu-fixed"); // menghapus class menu-fixed
+    menu.classList.remove("menu-fixed"); // hapus class menu-fixed
   }
 };
 
 // ## SIDEBAR ##
-// Ambil elemen tombol sidebar dan juga content dari sidebar
-const sidebarBtn = document.querySelector(".sidebar-btn span");
-const sidebarContent = document.querySelector(".sidebar-content");
+const sidebarBtn = document.querySelector(".sidebar-btn span"); // ambil elemen span yang berada di child elemen class sidebar-btn
+const sidebarContent = document.querySelector(".sidebar-content"); // ambil elemen dengan nama class sidebar-content
 
 // Ketika tombol sidebar diclick
 sidebarBtn.onclick = function () {
@@ -31,15 +30,32 @@ document.onclick = function (e) {
   }
 };
 
-// ## HERO BACKGROUND ##
-var bg = document.querySelector(".hero"); // ambil elemen class hero
+// ## HERO BACKGROUND CHANGE ##
+var bg = document.querySelector(".hero"); // ambil elemen dengan nama class hero
 var url = window.location.href; // ambil url dari halaman saat ini
 var fileName = url.split(/(\\|\/)/g).pop(); // lakukan split sehingga mendapatkan nama file nya saja
+
 // Apabila fileName nya form.html
 if (fileName == "form.html") {
-  bg.style.backgroundImage = "url(img/header2.jpg)"; // ubah background
+  bg.style.backgroundImage = "url(img/header2.jpg)"; // ubah style backgroundImage
 } else if (fileName == "evaluation.html") {
   bg.style.backgroundImage = "url(img/header3.jpg)";
 } else if (fileName == "about.html") {
   bg.style.backgroundImage = "url(img/header4.jpg)";
 }
+
+// ## MENU IN ACTIVE ##
+const homeLink = document.querySelector(".home-link"); // ambil elemen dengan nama class home-link
+const accomodationLink = document.querySelector(".accomodation-link"); // ambil elemen dengan nama class accomodation-link
+
+// Ketika homeLink diclick
+homeLink.onclick = function () {
+  homeLink.classList.add("active"); // tambah class active pada elemen homeLink
+  accomodationLink.classList.remove("active"); // hapus class active pada elemen accomodationLink
+};
+
+// Ketika accomodationLink diclick
+accomodationLink.onclick = function () {
+  homeLink.classList.remove("active"); // hapus class active pada elemen homeLink
+  accomodationLink.classList.add("active"); // tambah class active pada elemen accomodationLink
+};
