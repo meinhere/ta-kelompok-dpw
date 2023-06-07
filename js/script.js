8fd64e32b4872ce1286c0cc169d9a12cc1c32fec
Ranwiesiel <ronggofc@gmail.com>
2023-06-07 16:06:20 +0700

    mengubah & menambahkan link source di halaman about us

--- js/script.js
+++ js/script.js
@@ -37,5 +37,9 @@ var url = window.location.href; // ambil url dari halaman saat ini
 var fileName = url.split(/(\\|\/)/g).pop(); // lakukan split sehingga mendapatkan nama file nya saja
 // Apabila fileName nya form.html
 if (fileName == "form.html") {
-  bg.style.background = "url(img/header2.jpg)"; // ubah background
+  bg.style.backgroundImage = "url(img/header2.jpg)"; // ubah background
+} else if (fileName == "evaluation.html") {
+  bg.style.backgroundImage = "url(img/header3.jpg)";
+} else if (fileName == "about.html") {
+  bg.style.backgroundImage = "url(img/header4.jpg)";
 }
